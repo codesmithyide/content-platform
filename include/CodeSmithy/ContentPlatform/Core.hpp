3f53d65a74dbf6ab22cd3c86bd836fdaf7133d67
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-12 00:53:16 +0100

    Add files

---
+++ include/CodeSmithy/ContentPlatform/Core.hpp
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/codesmithyide/content-platform/blob/main/LICENSE.txt
+*/
+
+#ifndef _CODESMITHY_CONTENTPLATFORM_CORE_HPP_
+#define _CODESMITHY_CONTENTPLATFORM_CORE_HPP_
+
+#include "Core/WebServer/WebServer.hpp"
+
+#endif
