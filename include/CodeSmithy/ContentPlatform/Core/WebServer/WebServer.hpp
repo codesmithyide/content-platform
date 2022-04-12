bda2805eff823e82b3d7e6d032b9b821f2dd9c96
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-12 01:17:28 +0100

    Add WebServer class

--- include/CodeSmithy/ContentPlatform/Core/WebServer/WebServer.hpp
+++ include/CodeSmithy/ContentPlatform/Core/WebServer/WebServer.hpp
@@ -7,4 +7,18 @@
 #ifndef _CODESMITHY_CONTENTPLATFORM_CORE_WEBSERVER_WEBSERVER_HPP_
 #define _CODESMITHY_CONTENTPLATFORM_CORE_WEBSERVER_WEBSERVER_HPP_
 
+#include <Nemu/WebFramework.hpp>
+
+namespace CodeSmithy
+{
+namespace ContentPlatform
+{
+
+class WebServer
+{
+};
+
+}
+}
+
 #endif
