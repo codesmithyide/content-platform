2e338dd30e6db84d2b557912ddc891064f2bb27e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-12 02:27:41 +0100

    Start implementing web server (#5)

---
+++ include/CodeSmithy/ContentPlatform/Core/linkoptions.hpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/codesmithyide/content-platform/blob/main/LICENSE.txt
+*/
+
+#ifndef _CODESMITHY_CONTENTPLATFORM_CORE_LINKOPTIONS_HPP_
+#define _CODESMITHY_CONTENTPLATFORM_CORE_LINKOPTIONS_HPP_
+
+#ifdef _DEBUG
+#pragma comment(lib, "CodeSmithyContentPlatformCore-d.lib")
+#else
+#pragma comment(lib, "CodeSmithyContentPlatformCore.lib")
+#endif
+
+#endif
--- web-server/src/main.cpp
+++ web-server/src/main.cpp
@@ -4,7 +4,29 @@
     See https://github.com/codesmithyide/content-platform/blob/main/LICENSE.txt
 */
 
+#include <CodeSmithy/ContentPlatform/Core.hpp>
+#include <Ishiko/Logging.hpp>
+#include <iostream>
+#include <memory>
+
+using namespace CodeSmithy::ContentPlatform;
+
 int main(int argc, char* argv[])
 {
-    return 0;
+    try
+    {
+        // Create a log that sends its output to the console.
+        Ishiko::StreamLoggingSink sink(std::cout);
+        Ishiko::Logger log(sink);
+
+        WebServer server(log);
+        server.run();
+
+        return 0;
+    }
+    catch (...)
+    {
+        // TODO
+        return 0;
+    }
 }
