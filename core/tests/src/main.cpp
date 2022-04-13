fd22c0a7dd9479a1be6b16f6c8ba9afc7585d56c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-13 15:29:40 +0100

    Add tests

--- core/tests/src/main.cpp
+++ core/tests/src/main.cpp
@@ -4,6 +4,8 @@
     See https://github.com/codesmithyide/content-platform/blob/main/LICENSE.txt
 */
 
+#include "WebServerTests.hpp"
+#include "CodeSmithy/ContentPlatform/Core/linkoptions.hpp"
 #include <Ishiko/TestFramework/Core.hpp>
 
 using namespace Ishiko;
@@ -11,6 +13,9 @@ using namespace Ishiko;
 int main(int argc, char* argv[])
 {
     TestHarness theTestHarness("CodeSmithyContentPlatformCore Library Tests");
+
+    TestSequence& theTests = theTestHarness.tests();
+    theTests.append<WebServerTests>();
 
     return theTestHarness.run();
 }
