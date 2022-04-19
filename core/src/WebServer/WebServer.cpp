681bbdd36af2b3d65cce2b420e89952e9c828233
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-19 23:01:32 +0100

    Updates

--- core/src/WebServer/WebServer.cpp
+++ core/src/WebServer/WebServer.cpp
@@ -8,12 +8,11 @@
 
 using namespace CodeSmithy::ContentPlatform;
 
-WebServer::WebServer(Ishiko::Logger& logger)
+WebServer::WebServer(const std::string& contentRootDir, Ishiko::Logger& logger)
     : m_app(std::make_shared<Nemu::SingleConnectionWebServer>(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http, logger),
         logger)
 {
-   m_app.routes().append(Nemu::Route("/*",
-        std::make_shared<Nemu::FileSystemWebRequestHandler>("D:\\www")));
+   m_app.routes().append(Nemu::Route("/*", std::make_shared<Nemu::FileSystemWebRequestHandler>(contentRootDir)));
 }
 
 void WebServer::run()
