03a4a43fc264b9f85a0e8b129850ba22ceb7502c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-19 18:48:03 +0100

    Updates

--- core/src/WebServer/WebServer.cpp
+++ core/src/WebServer/WebServer.cpp
@@ -9,22 +9,11 @@
 using namespace CodeSmithy::ContentPlatform;
 
 WebServer::WebServer(Ishiko::Logger& logger)
-    : m_app(std::make_shared<Nemu::SingleConnectionWebServer>(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http),
+    : m_app(std::make_shared<Nemu::SingleConnectionWebServer>(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http, logger),
         logger)
 {
-    /*Route::ptr route = std::make_shared<Route>(
-            "/",
-            std::make_shared<FileSystemRequestHandler>("../../Data/")
-            );*/
-
-    m_app.routes().append(Nemu::Route("/",
-        std::make_shared<Nemu::FunctionWebRequestHandler>(
-            [](const Nemu::WebRequest& request, Nemu::WebResponseBuilder& response, void* handlerData, Ishiko::Logger& logger)
-            {
-                // TODO: this doesn't work as the user would be left with specifying all the right headers. Do I assume some default?
-                response.setStatus(Ishiko::HTTPStatusCode::ok);
-                response.body() = "Hello nemu.wf";
-            })));
+   m_app.routes().append(Nemu::Route("/*",
+        std::make_shared<Nemu::FileSystemWebRequestHandler>("D:\\www")));
 }
 
 void WebServer::run()
