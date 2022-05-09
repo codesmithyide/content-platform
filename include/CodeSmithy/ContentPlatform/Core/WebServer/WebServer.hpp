5ef683813445cf57e40448cf8d3e424d3a774502
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-09 16:00:46 +0100

    Support views with layout templates (#15)

--- include/CodeSmithy/ContentPlatform/Core/WebServer/WebServer.hpp
+++ include/CodeSmithy/ContentPlatform/Core/WebServer/WebServer.hpp
@@ -7,6 +7,7 @@
 #ifndef _CODESMITHY_CONTENTPLATFORM_CORE_WEBSERVER_WEBSERVER_HPP_
 #define _CODESMITHY_CONTENTPLATFORM_CORE_WEBSERVER_WEBSERVER_HPP_
 
+#include "../Presentation.hpp"
 #include <Ishiko/Logging.hpp>
 #include <Nemu.hpp>
 #include <memory>
@@ -19,8 +20,7 @@ namespace ContentPlatform
 class WebServer
 {
 public:
-    // TODO: contentRootDir is probably not flexible enough but it will do for now
-    WebServer(const std::string& contentRootDir, Ishiko::Logger& logger);
+    WebServer(const Presentation& presentation, Ishiko::Logger& logger);
 
     void run();
     void stop();
