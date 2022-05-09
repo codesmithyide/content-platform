c87c4813abc044d21159c7c2cc59809768c3c411
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-09 12:28:22 +0100

    Updates

--- core/src/WebServer/WebServer.cpp
+++ core/src/WebServer/WebServer.cpp
@@ -9,10 +9,35 @@
 using namespace CodeSmithy::ContentPlatform;
 
 WebServer::WebServer(const std::string& contentRootDir, Ishiko::Logger& logger)
-    : m_app(std::make_shared<Nemu::SingleConnectionWebServer>(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http, logger),
+    : m_app(
+        std::make_shared<Nemu::SingleConnectionWebServer>(Ishiko::TCPServerSocket::AllInterfaces, Ishiko::Port::http,
+            logger),
         logger)
 {
-   m_app.routes().append(Nemu::Route("/*", std::make_shared<Nemu::FileSystemWebRequestHandler>(contentRootDir)));
+    // Set the mustache engine as the default template engine
+    // TODO: get from config
+    const std::string templatesRootDir = "${CODESMITHYIDE}/content-platform-themes/default/templates";
+    const std::string layoutsRootDir = "${CODESMITHYIDE}/content-platform-themes/default/layout";
+    m_app.views().add(
+        std::make_shared<Nemu::MustacheTemplateEngine>(
+            Nemu::MustacheTemplateEngine::Options(templatesRootDir, layoutsRootDir)));
+
+    m_app.routes().append(
+        Nemu::Route("/*",
+            std::make_shared<Nemu::FunctionWebRequestHandler>(
+                [](const Nemu::WebRequest& request, Nemu::WebResponseBuilder& response, void* handlerData,
+                    Ishiko::Logger& logger)
+                {
+                    Nemu::ViewContext context;
+                    // TODO: get project from config
+                    context["codesmithy_page_title"] = "My Project";
+                    std::string templatePath = request.url().path();
+                    if (templatePath == "/")
+                    {
+                        templatePath = "index.html";
+                    }
+                    response.view(templatePath, context, "page.html");
+                })));
 }
 
 void WebServer::run()
