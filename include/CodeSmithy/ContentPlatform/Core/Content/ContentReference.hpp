59e364ad1b63d41111fdd49c1c7a2fa3280d51d5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-31 15:43:48 +0100

    Add files

---
+++ include/CodeSmithy/ContentPlatform/Core/Content/ContentReference.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/codesmithyide/content-platform/blob/main/LICENSE.txt
+*/
+
+#ifndef _CODESMITHY_CONTENTPLATFORM_CORE_CONTENT_CONTENTREFERENCE_HPP_
+#define _CODESMITHY_CONTENTPLATFORM_CORE_CONTENT_CONTENTREFERENCE_HPP_
+
+#endif
