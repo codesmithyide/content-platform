c9ca78a52de4e2f2b4b5758c1f94679b15ffb1a9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-02 13:52:23 +0100

    Fix GCC build

--- include/CodeSmithy/ContentPlatform/Core/WebServer/WebServer.hpp
+++ include/CodeSmithy/ContentPlatform/Core/WebServer/WebServer.hpp
@@ -8,7 +8,7 @@
 #define _CODESMITHY_CONTENTPLATFORM_CORE_WEBSERVER_WEBSERVER_HPP_
 
 #include "../Content/Content.hpp"
-#include "../Presentation.hpp"
+#include "../Presentation/Presentation.hpp"
 #include <boost/filesystem.hpp>
 #include <Ishiko/Configuration.hpp>
 #include <Ishiko/Logging.hpp>
