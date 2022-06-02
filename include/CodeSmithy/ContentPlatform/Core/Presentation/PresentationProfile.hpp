8fb8906a9eab722e9c1e8609dcf442e187d0e43d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-02 18:19:45 +0100

    Delete obsolete file

---
+++ include/CodeSmithy/ContentPlatform/Core/Presentation/PresentationProfile.hpp
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/codesmithyide/content-platform/blob/main/LICENSE.txt
+*/
+
+#ifndef _CODESMITHY_CONTENTPLATFORM_CORE_PRESENTATION_PRESENTATIONPROFILE_HPP_
+#define _CODESMITHY_CONTENTPLATFORM_CORE_PRESENTATION_PRESENTATIONPROFILE_HPP_
+
+namespace CodeSmithy
+{
+namespace ContentPlatform
+{
+
+class PresentationProfile
+{
+};
+
+}
+}
+
+#endif
