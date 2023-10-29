e2495bdbf197caac57af7f743b427d97af33ecd4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-10-29 23:01:46 +0000

    Add web server example

--- web-server/src/main.cpp
+++ web-server/src/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2022 Xavier Leclercq
+    Copyright (c) 2022-2023 Xavier Leclercq
     Released under the MIT License
     See https://github.com/codesmithyide/content-platform/blob/main/LICENSE.txt
 */
@@ -14,6 +14,11 @@ using namespace CodeSmithy::ContentPlatform;
 
 int main(int argc, char* argv[])
 {
+    // TODO: guarantee this doesn't throw exceptions/always succeed, can we do that?
+    // Create a log that sends its output to stdout and stderr.
+    Ishiko::StandardStreamsLoggingSink sink;
+    Ishiko::Logger log(sink);
+
     try
     {
         WebServer::CommandLineSpecification commandLineSpec;
@@ -21,10 +26,6 @@ int main(int argc, char* argv[])
         Ishiko::CommandLineParser parser;
         parser.parse(commandLineSpec, argc, argv, configuration);
         // TODO: validate the configuration at some point
-
-        // Create a log that sends its output to stdout and stderr.
-        Ishiko::StandardStreamsLoggingSink sink;
-        Ishiko::Logger log(sink);
 
         WebServer::Configuration webServerConfiguration(configuration);
         log.setLevel(webServerConfiguration.logLevel());
@@ -34,9 +35,15 @@ int main(int argc, char* argv[])
 
         return 0;
     }
+    catch (const std::exception& e)
+    {
+        // TODO: proper error codes
+        log.error(e.what());
+        return -1;
+    }
     catch (...)
     {
-        // TODO
-        return 0;
+        // TODO: proper error codes
+        return -1;
     }
 }
